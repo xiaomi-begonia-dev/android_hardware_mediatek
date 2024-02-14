bbe62376c1d52f765888dafb872c83b9ae47b88b
bengris32 <bengris32@protonmail.ch>
2024-02-14 20:33:35 +0000

    aidl: usb: Unconditionally switch back to DRP after role swap
    Change-Id: I5bdb054ebbb8e073740bb8adc4945f56edb0191f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- aidl/usb/Usb.cpp
+++ aidl/usb/Usb.cpp
@@ -289,8 +289,7 @@ bool switchMode(const string &portName, const PortRole &in_role, struct Usb *usb
         pthread_mutex_unlock(&usb->mPartnerLock);
     }
 
-    if (!roleSwitch)
-        switchToDrp(string(portName.c_str()));
+    switchToDrp(string(portName.c_str()));
 
     return roleSwitch;
 }
